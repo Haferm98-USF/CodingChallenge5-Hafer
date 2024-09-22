074e236858cab5aa79ee5ca84d212c2e816aa4b8
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-09-22 14:57:57 -0400

    Initialize orders array

--- order_management_system.js
+++ order_management_system.js
@@ -7,3 +7,13 @@ let inventory =
     {name: 'Cappacino', price: 7.00, quantity: 50},
     {name: 'Muffin', price: 2.00, quantity: 77},
 ];
+
+//Task 2: Create an Orders Array of Order Objects
+
+let orders = [];
+orders.push
+({
+customerName: 'Matt',
+items: [Latte, Muffin],
+status: 'Pending'
+})
